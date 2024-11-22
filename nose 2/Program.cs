9c3688dece2d091690112b30021018d8304947b3
Haroon Irfan <haroon.irfan@elev.ga.ntig.se>
2024-11-22 14:06:37 +0100

    first commit

---
+++ nose 2/Program.cs
@@ -0,0 +1,42 @@
+﻿// List<string> toys = ["Teddy", "DUCK", "Cars", "Guns", "Knifes"];
+
+// // foreach (string toy in toys)
+// // {
+// //   Console.WriteLine(toy);
+// // }
+
+// // Console.ReadLine();
+
+// List<string> toys = ["Teddy", "DUCK", "Cars", "Guns", "Knifes"];
+// List<string> names = ["Haroon", "Mostafa", "John", "DAC", "CHICKEN WING"];
+// List<int> num = [];
+// for (int i = 0; i < 5; i++)
+// {
+//     num.Add(Random.Shared.Next(0, 10));
+//     Console.WriteLine($"{names[i]} ger {toys[i]} betyget {num[i]}");
+// }
+
+// Console.ReadLine();
+
+
+
+List<string> cities = new List<string>();
+
+
+// while (true)
+// {
+// Console.WriteLine("Write names of cities");
+// string name = Console.ReadLine().ToLower();
+// if (name == "exit")
+// {
+//     break;
+// }
+// cities.Add(name );
+// }
+
+// for (int i = 0; i < cities.Count; i++)
+// {
+//     Console.WriteLine(cities[i]);
+// }
+// Console.ReadLine();
+
